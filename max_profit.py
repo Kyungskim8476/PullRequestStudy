8efbcb3b4af56203ba3407abbb67d003883ee3da
kyungskim-kit <kyungskim.kit@gmail.com>
2022-11-21 19:14:45 +0900

    modify hello.c and max_profit.py by kyungskim-kit

--- max_profit.py
+++ max_profit.py
@@ -1,9 +1,11 @@
-def maxProfit_bruteforce (prices):
-   max_price = 0
+# This code computes a maximum profit value...
+import sys
 
+def maxProfit_bruteforce (prices):
+   max_price = -sys.maximum
    for i, price in enumerate(prices):
        for j in range(i, len(prices)):
            max_price = max(prices[j] - price, max_price)
 
-   return
+   return max_price
 

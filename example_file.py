bac8a01245fa397f8c2b6ea5de03e574154984b7
Kyungskim8476 <97086141+Kyungskim8476@users.noreply.github.com>
2022-11-21 18:48:27 +0900

    Add files via upload

---
+++ example_file.py
@@ -0,0 +1,13 @@
+# print function
+def print_hello():
+    print("Hello Python!")
+    print("This is written using Nano.");
+
+# division function
+def div(x, y):
+    if y == 0 :
+        print("Error!")
+	return
+
+    return x / y
+
